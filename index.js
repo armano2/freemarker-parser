51622da06a3aea2415f5bfdd069582b35701662e
Armano <armano2@users.noreply.github.com>
2017-09-03 22:05:52 +0200

    Fix index.js

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ class NodeError extends Error {
         Object.setPrototypeOf(this, NodeError.prototype);
     }
 }
-//# sourceMappingURL=NodeError.js.map
 
 var ENodeType;
 (function (ENodeType) {
@@ -43,7 +42,6 @@ const whitespaces = [
 function isWhitespace(char) {
     return char === ' ' || char === '\t' || char === '\r' || char === '\n';
 }
-//# sourceMappingURL=Symbols.js.map
 
 var NodeNames;
 (function (NodeNames) {
@@ -68,7 +66,6 @@ var NodeNames;
     NodeNames["Break"] = "Break";
     NodeNames["ConditionElse"] = "ConditionElse";
 })(NodeNames || (NodeNames = {}));
-//# sourceMappingURL=Node.js.map
 
 const directives = {
     if: NodeNames.Condition,
@@ -87,7 +84,6 @@ const directives = {
     default: NodeNames.SwitchDefault,
     break: NodeNames.Break,
 };
-//# sourceMappingURL=Tokens.js.map
 
 function cAssign(params, start, end) {
     return { type: NodeNames.Assign, start, end, params };
@@ -140,7 +136,6 @@ function cSwitchDefault(start, end) {
 function cBreak(start, end) {
     return { type: NodeNames.Break, start, end };
 }
-//# sourceMappingURL=Node.js.map
 
 class ParamError extends SyntaxError {
     constructor(message, index) {
@@ -150,7 +145,6 @@ class ParamError extends SyntaxError {
         Object.setPrototypeOf(this, ParamError.prototype);
     }
 }
-//# sourceMappingURL=ParamError.js.map
 
 var ParamNames;
 (function (ParamNames) {
@@ -164,7 +158,6 @@ var ParamNames;
     ParamNames["LogicalExpression"] = "LogicalExpression";
     ParamNames["ArrayExpression"] = "ArrayExpression";
 })(ParamNames || (ParamNames = {}));
-//# sourceMappingURL=Params.js.map
 
 const PERIOD_CODE = 46;
 const COMMA_CODE = 44;
@@ -640,7 +633,6 @@ class ParamsParser {
         };
     }
 }
-//# sourceMappingURL=ParamsParser.js.map
 
 function parseParams(tokenParams) {
     const parser = new ParamsParser();
@@ -650,7 +642,6 @@ function parseParams(tokenParams) {
     }
     return params;
 }
-//# sourceMappingURL=Params.js.map
 
 function addToNode(parent, child) {
     switch (parent.type) {
@@ -850,7 +841,6 @@ function cToken(type, start, end, text, params = [], isClose = false) {
         isClose,
     };
 }
-//# sourceMappingURL=Token.js.map
 
 class Tokenizer {
     constructor() {
@@ -1066,9 +1056,6 @@ class Parser {
         return { ast, tokens };
     }
 }
-//# sourceMappingURL=Parser.js.map
-
-//# sourceMappingURL=index.js.map
 
 exports.Parser = Parser;
 exports.Tokenizer = Tokenizer;
