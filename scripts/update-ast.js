f4b58eda38b7989e589660c77308ff5868d05f1c
Armano <armano2@users.noreply.github.com>
2018-10-23 01:07:47 +0200

    Update build to make it complain with github checks

--- scripts/update-ast.js
+++ scripts/update-ast.js
@@ -1,33 +1,34 @@
 const freemarker = require('../dist/index')
 const fs = require('fs')
 const path = require('path')
-const util = require('util')
-const glob = util.promisify(require('glob'));
+const glob = require('glob')
 
 const parser = new freemarker.Parser()
 
-const rootDir = path.join(__dirname, '..', 'test', 'resource')
+const testsPath = path.join(__dirname, '..', 'test', 'resource')
 
 function stringify (text) {
   return JSON.stringify(text, null, 2)
 }
 
-glob('./**/*.ftl', { cwd: rootDir, nodir: true, absolute: true })
-  .then((files) => {
-    for (const file of files) {
-      fs.readFile(file, 'utf8', (err, template) => {
-        if (err) {
-          throw new Error(err)
-        }
-        const dirname = path.dirname(file)
-        const basename = path.basename(file).replace(path.extname(file), '')
+glob('./**/*.ftl', { cwd: testsPath, nodir: true, absolute: true }, (e, files) => {
+  if (e) {
+    throw e
+  }
+  for (const file of files) {
+    fs.readFile(file, 'utf8', (err, template) => {
+      if (err) {
+        throw new Error(err)
+      }
+      const dirname = path.dirname(file)
+      const basename = path.basename(file).replace(path.extname(file), '')
 
-        console.log(`Updating data ${basename}`)
-        console.log(' file:', path.relative(rootDir, file))
+      console.log(`Updating data ${basename}`)
+      console.log(' file:', path.relative(testsPath, file))
 
-        const data = parser.parse(template)
-        fs.writeFileSync(path.join(dirname, `${basename}-tokens.json`), stringify(data.tokens, null, 2))
-        fs.writeFileSync(path.join(dirname, `${basename}-ast.json`), stringify(data.ast, null, 2))
-      })
-    }
-  })
+      const data = parser.parse(template)
+      fs.writeFileSync(path.join(dirname, `${basename}-tokens.json`), stringify(data.tokens, null, 2))
+      fs.writeFileSync(path.join(dirname, `${basename}-ast.json`), stringify(data.ast, null, 2))
+    })
+  }
+})
