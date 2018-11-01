9ef0ed7f5ed5e14843a0563d397c06427858b677
Armano <armano2@users.noreply.github.com>
2018-11-01 04:10:22 +0100

    Enforce Literal in #outputFormat

--- src/nodes/OutputFormatNode.ts
+++ src/nodes/OutputFormatNode.ts
@@ -3,6 +3,8 @@ import {IExpression} from '../interface/Params'
 import { IToken } from '../interface/Tokens'
 import { paramParser } from '../utils/Params'
 import AbstractBodyNode from './abstract/AbstractBodyNode'
+import ParamNames from '../enum/ParamNames'
+import ParseError from '../errors/ParseError'
 
 export default class OutputFormatNode extends AbstractBodyNode {
   public params? : IExpression
@@ -11,5 +13,8 @@ export default class OutputFormatNode extends AbstractBodyNode {
     super(NodeNames.OutputFormat, token)
     this.params = paramParser(token.start, token.end, token.params)
     this.body = []
+    if (!this.params || this.params.type !== ParamNames.Literal) {
+      throw new ParseError(`Invalid parameters in ${this.type}`, token)
+    }
   }
 }
