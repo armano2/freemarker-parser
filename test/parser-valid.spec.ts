7dd465d3f79928f1cbefa515ced94ce3075b1b9e
Armano <armano2@users.noreply.github.com>
2019-12-24 00:12:41 +0100

    migrate tests to jest snapshots (#18)

--- test/parser-valid.spec.ts
+++ test/parser-valid.spec.ts
@@ -10,60 +10,37 @@ const baseDir = path.join(__dirname, '..');
 
 const testsPath = path.join(__dirname, 'resource', 'valid');
 
-function cleanup(data: any) {
-  return JSON.parse(JSON.stringify(data));
+const files = glob.sync('./**/*.ftl', {
+  cwd: testsPath,
+  nodir: true,
+  absolute: true,
+});
+
+for (const file of files) {
+  describe(path.basename(file), () => {
+    const template = fs.readFileSync(file, 'utf8');
+    const data = parser.parse(template);
+
+    it('should have no errors', () => {
+      if (data.ast.errors) {
+        for (const error of data.ast.errors) {
+          assert.fail(
+            `${error.message}\n\tfile:.\\${path.relative(baseDir, file)}:${
+              error.loc
+                ? `${error.loc.start.column}:${error.loc.start.line}`
+                : '0:0'
+            }`,
+          );
+        }
+      }
+    });
+
+    it('should have correct tokens', () => {
+      expect(data.tokens).toMatchSnapshot();
+    });
+
+    it('should have correct ast', () => {
+      expect(data.ast).toMatchSnapshot();
+    });
+  });
 }
-
-glob(
-  './**/*.ftl',
-  { cwd: testsPath, nodir: true, absolute: true },
-  (e: Error | null, files: string[]) => {
-    if (e) {
-      throw e;
-    }
-    for (const file of files) {
-      describe(file, () => {
-        const dirname = path.dirname(file);
-        const basename = path.basename(file).replace(path.extname(file), '');
-
-        const template = fs.readFileSync(file, 'utf8');
-        const data = parser.parse(template);
-
-        it('should have no errors', () => {
-          if (data.ast.errors) {
-            for (const error of data.ast.errors) {
-              assert.fail(
-                `${error.message}\n\tfile:.\\${path.relative(baseDir, file)}:${
-                  error.loc
-                    ? `${error.loc.start.column}:${error.loc.start.line}`
-                    : '0:0'
-                }`,
-              );
-            }
-          }
-        });
-
-        it('should have correct tokens', () => {
-          const code = JSON.parse(
-            fs.readFileSync(
-              path.join(dirname, `${basename}-tokens.json`),
-              'utf8',
-            ),
-          );
-          assert.deepStrictEqual(
-            cleanup(data.tokens),
-            code,
-            'tokens do not match',
-          );
-        });
-
-        it('should have correct ast', () => {
-          const code = JSON.parse(
-            fs.readFileSync(path.join(dirname, `${basename}-ast.json`), 'utf8'),
-          );
-          assert.deepStrictEqual(cleanup(data.ast), code, 'ast do not match');
-        });
-      });
-    }
-  },
-);
--- test/tokenizer.spec.ts
+++ test/tokenizer.spec.ts
@@ -1,7 +1,5 @@
 import { Tokenizer } from '../src';
 import { IToken } from '../src/interface/Tokens';
-import { ENodeType } from '../src/Symbols';
-import Tester from './testers/TokenizerTester';
 
 const tokenizer = new Tokenizer();
 
@@ -12,266 +10,128 @@ function parse(text: string): IToken[] {
 describe('parsing directives', () => {
   it('no arguments', () => {
     const tokens = parse('<#foo>');
-    Tester.instance(tokens)
-      .hasCount(1)
-      .isType(ENodeType.OpenDirective)
-      .hasNoParams();
+    expect(tokens).toMatchSnapshot();
   });
-  it('no arguments, self closing', () => {
+
+  it('no arguments, self-closing', () => {
     const tokens = parse('<#foo/>');
-    Tester.instance(tokens)
-      .hasCount(1)
-      .isType(ENodeType.OpenDirective)
-      .hasNoParams();
+    expect(tokens).toMatchSnapshot();
   });
+
   it('many, no arguments', () => {
     const tokens = parse('<#foo><#foo>');
-    Tester.instance(tokens)
-      .hasCount(2)
-      .isType(ENodeType.OpenDirective)
-      .hasNoParams()
-      .nextToken()
-      .isType(ENodeType.OpenDirective)
-      .hasNoParams();
+    expect(tokens).toMatchSnapshot();
   });
+
   it('many, no arguments, with text', () => {
     const tokens = parse('foo<#foo>foo<#foo>foo');
-    Tester.instance(tokens)
-      .hasCount(5)
-      .isType(ENodeType.Text)
-      .hasNoParams()
-      .hasText('foo')
-      .nextToken()
-      .isType(ENodeType.OpenDirective)
-      .hasNoParams()
-      .nextToken()
-      .isType(ENodeType.Text)
-      .hasNoParams()
-      .hasText('foo')
-      .nextToken()
-      .isType(ENodeType.OpenDirective)
-      .hasNoParams()
-      .nextToken()
-      .isType(ENodeType.Text)
-      .hasNoParams()
-      .hasText('foo');
+    expect(tokens).toMatchSnapshot();
   });
+
   it('no arguments, with close tag', () => {
     const tokens = parse('<#foo></#foo>');
-    Tester.instance(tokens)
-      .hasCount(2)
-      .isType(ENodeType.OpenDirective)
-      .hasNoParams()
-      .nextToken()
-      .isType(ENodeType.CloseDirective)
-      .hasNoParams();
+    expect(tokens).toMatchSnapshot();
   });
+
   it('with arguments', () => {
     const tokens = parse('<#foo bar>');
-    Tester.instance(tokens)
-      .hasCount(1)
-      .isType(ENodeType.OpenDirective)
-      .hasParams('bar');
+    expect(tokens).toMatchSnapshot();
   });
+
   it('many, with arguments', () => {
     const tokens = parse('<#foo bar><#foo bar test><#foo bar less>');
-    Tester.instance(tokens)
-      .hasCount(3)
-      .isType(ENodeType.OpenDirective)
-      .hasParams('bar')
-      .nextToken()
-      .isType(ENodeType.OpenDirective)
-      .hasParams('bar test')
-      .nextToken()
-      .isType(ENodeType.OpenDirective)
-      .hasParams('bar less');
+    expect(tokens).toMatchSnapshot();
   });
 });
 
 describe('parsing macros', () => {
   it('no arguments', () => {
     const tokens = parse('<@foo>');
-    Tester.instance(tokens)
-      .hasCount(1)
-      .isType(ENodeType.OpenMacro)
-      .hasNoParams();
+    expect(tokens).toMatchSnapshot();
   });
-  it('no arguments, self closing', () => {
+
+  it('no arguments, self-closing', () => {
     const tokens = parse('<@foo/>');
-    Tester.instance(tokens)
-      .hasCount(1)
-      .isType(ENodeType.OpenMacro)
-      .hasNoParams();
+    expect(tokens).toMatchSnapshot();
   });
+
   it('many, no arguments', () => {
     const tokens = parse('<@foo><@foo>');
-    Tester.instance(tokens)
-      .hasCount(2)
-      .isType(ENodeType.OpenMacro)
-      .hasNoParams()
-      .nextToken()
-      .isType(ENodeType.OpenMacro)
-      .hasNoParams();
+    expect(tokens).toMatchSnapshot();
   });
+
   it('many, no arguments, with text', () => {
     const tokens = parse('foo<@foo>foo<@foo>foo');
-    Tester.instance(tokens)
-      .hasCount(5)
-      .isType(ENodeType.Text)
-      .hasNoParams()
-      .hasText('foo')
-      .nextToken()
-      .isType(ENodeType.OpenMacro)
-      .hasNoParams()
-      .nextToken()
-      .isType(ENodeType.Text)
-      .hasNoParams()
-      .hasText('foo')
-      .nextToken()
-      .isType(ENodeType.OpenMacro)
-      .hasNoParams()
-      .nextToken()
-      .isType(ENodeType.Text)
-      .hasNoParams()
-      .hasText('foo');
+    expect(tokens).toMatchSnapshot();
   });
+
   it('no arguments, with close tag', () => {
     const tokens = parse('<@foo></@foo>');
-    Tester.instance(tokens)
-      .hasCount(2)
-      .isType(ENodeType.OpenMacro)
-      .hasNoParams()
-      .nextToken()
-      .isType(ENodeType.CloseMacro)
-      .hasNoParams();
+    expect(tokens).toMatchSnapshot();
   });
+
   it('with arguments', () => {
     const tokens = parse('<@foo bar>');
-    Tester.instance(tokens)
-      .hasCount(1)
-      .isType(ENodeType.OpenMacro)
-      .hasParams('bar');
+    expect(tokens).toMatchSnapshot();
   });
+
   it('many, with arguments', () => {
     const tokens = parse('<@foo bar><@foo bar test><@foo bar less>');
-    Tester.instance(tokens)
-      .hasCount(3)
-      .isType(ENodeType.OpenMacro)
-      .hasParams('bar')
-      .nextToken()
-      .isType(ENodeType.OpenMacro)
-      .hasParams('bar test')
-      .nextToken()
-      .isType(ENodeType.OpenMacro)
-      .hasParams('bar less');
+    expect(tokens).toMatchSnapshot();
   });
 });
 
 describe('parsing comments', () => {
   it('coment with text', () => {
     const tokens = parse('<#--  <@d></@d>  -->');
-    Tester.instance(tokens)
-      .hasCount(1)
-      .isType(ENodeType.Comment)
-      .hasNoParams()
-      .hasText('  <@d></@d>  ');
+    expect(tokens).toMatchSnapshot();
   });
+
   it('coment in directive', () => {
     const tokens = parse('<#foo><#--  foo  --></#foo>');
-    Tester.instance(tokens)
-      .hasCount(3)
-      .isType(ENodeType.OpenDirective)
-      .hasNoParams()
-      .nextToken()
-      .isType(ENodeType.Comment)
-      .hasNoParams()
-      .hasText('  foo  ')
-      .nextToken()
-      .isType(ENodeType.CloseDirective)
-      .hasNoParams();
+    expect(tokens).toMatchSnapshot();
   });
 });
 
 describe('parsing text', () => {
   it('empty text', () => {
     const tokens = parse('');
-    Tester.instance(tokens).hasCount(0);
+    expect(tokens).toMatchSnapshot();
   });
+
   it('raw text', () => {
     const tokens = parse('<foo>');
-    Tester.instance(tokens)
-      .hasCount(1)
-      .isType(ENodeType.Text)
-      .hasNoParams()
-      .hasText('<foo>');
+    expect(tokens).toMatchSnapshot();
   });
+
   it('text after directive', () => {
     const tokens = parse('<#foo>foo');
-    Tester.instance(tokens)
-      .hasCount(2)
-      .isType(ENodeType.OpenDirective)
-      .hasNoParams()
-      .nextToken()
-      .isType(ENodeType.Text)
-      .hasNoParams()
-      .hasText('foo');
+    expect(tokens).toMatchSnapshot();
   });
+
   it('text before directive', () => {
     const tokens = parse('foo<#foo>');
-    Tester.instance(tokens)
-      .hasCount(2)
-      .isType(ENodeType.Text)
-      .hasNoParams()
-      .hasText('foo')
-      .nextToken()
-      .isType(ENodeType.OpenDirective)
-      .hasNoParams();
+    expect(tokens).toMatchSnapshot();
   });
 });
 
 describe('html', () => {
   it('simple', () => {
     const tokens = parse('<p>This is include-subdir.ftl</p>');
-    Tester.instance(tokens)
-      .hasCount(1)
-      .isType(ENodeType.Text)
-      .hasNoParams()
-      .hasText('<p>This is include-subdir.ftl</p>');
+    expect(tokens).toMatchSnapshot();
   });
+
   it('advance', () => {
     const tokens = parse(
       '<p>This is include-subdir.ftl</p><#include "include-subdir2.ftl">',
     );
-    Tester.instance(tokens)
-      .hasCount(2)
-      .isType(ENodeType.Text)
-      .hasNoParams()
-      .hasText('<p>This is include-subdir.ftl</p>')
-      .nextToken()
-      .isType(ENodeType.OpenDirective)
-      .hasParams('"include-subdir2.ftl"');
+    expect(tokens).toMatchSnapshot();
   });
 });
 
 describe('names', () => {
   it('directive', () => {
     const tokens = parse('<#foo/><#Foo/><#FoO/><#Fo_O/>');
-    Tester.instance(tokens)
-      .hasCount(4)
-      .isType(ENodeType.OpenDirective)
-      .hasNoParams()
-      .hasText('foo')
-      .nextToken()
-      .isType(ENodeType.OpenDirective)
-      .hasNoParams()
-      .hasText('Foo')
-      .nextToken()
-      .isType(ENodeType.OpenDirective)
-      .hasNoParams()
-      .hasText('FoO')
-      .nextToken()
-      .isType(ENodeType.OpenDirective)
-      .hasNoParams()
-      .hasText('Fo_O');
+    expect(tokens).toMatchSnapshot();
   });
 });
