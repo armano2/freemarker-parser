a027500123f00b57bfa3953291fa0b1c8521858f
Armano <armano2@users.noreply.github.com>
2020-02-01 21:47:10 +0100

    chore: upgrade jest and ts-eslint

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   testEnvironment: 'node',
+  preset: 'ts-jest',
   collectCoverage: false,
   testRegex: './test/.*\\.spec\\.ts$',
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   coverageReporters: ['text-summary', 'lcovonly'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
 };
